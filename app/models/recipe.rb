5f9f5d78ffd38b0d0d451b4ee0823dd8d52b0401
Sid23 <marchisio.simone@gmail.com>
2017-03-22 17:43:11 +0100

    association ok, start working on recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3, maximum: 30}
   validates :description, presence: true, length: {minimum: 10, maximum: 255}
+  validates :chef_id, presence: true
   belongs_to :chef
 end
