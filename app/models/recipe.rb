6525fec205ac0b6fa717a34bd8b5f1771d394a9f
Sid23 <marchisio.simone@gmail.com>
2017-03-22 17:09:43 +0100

    association 1:m

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3, maximum: 30}
   validates :description, presence: true, length: {minimum: 10, maximum: 255}
+  belongs_to :chef
 end
